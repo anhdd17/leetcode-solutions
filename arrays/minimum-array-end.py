0d7282c7663fd8758b1447343594f3929875c172
anhddhe170353 <anhddhe170353@fpt.edu.vn>
2024-11-10 00:06:26 +0700

    Add solution for Minimum Array End

---
+++ arrays/minimum-array-end.py
@@ -0,0 +1,12 @@
+def minEnd(self, n: int, x: int) -> int:
+    result = x
+    remaining = n - 1
+    position = 1
+
+    while remaining:
+        if not (x & position):
+            result |= (remaining & 1) * position
+            remaining >>= 1
+        position <<= 1
+
+    return result
